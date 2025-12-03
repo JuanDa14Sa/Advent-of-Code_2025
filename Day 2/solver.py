4c3fea22d389d1d88eb4b38a27ec9658fa72d280
JuanDa <coziestwharf@gmail.com>
2025-12-02 21:22:36 -0500

    Day 2 first part complete

--- Day 2/solver.py
+++ Day 2/solver.py
@@ -7,21 +7,29 @@ list_ids = []
 for line in lines:
     list_ids.append(line.split(','))
 
+match_counter = 0
+invalid_ids = []
+invalid_ids_sum = 0
 for id_range in list_ids[0]:
     start = int(id_range.split('-')[0])
     end = int(id_range.split('-')[1])
     id_range_list = range(start, end+1)
-    print(f'Checking {id_range}')
-    for number in id_range_list:  
-        print(f'Cheking {number}')
+    # print(f'Checking {id_range}')
+    for number in id_range_list:
         number_str = str(number)
-        tmp_str= number_str[0]
-        for i, digit in enumerate(number_str[1:]):
-            print(f'Searching {tmp_str}')
-            if tmp_str == number_str[i+1:i+len(tmp_str)+1]:
-                print(f'Found {tmp_str}')
-            else:
-                tmp_str+=number_str[i+1]
-                print(tmp_str)
+        # print(f'Cheking {number}')
+        half_str = number_str[len(number_str)//2::]
+        if len(number_str) % 2 == 1:
+            pass
+        else:
+            pattern = number_str[:len(number_str)//2]
+            if half_str == pattern:
+                # print(f'Match found for {pattern} in {half_str}')
+                match_counter+=1
+                invalid_ids.append(number)
+                invalid_ids_sum+=int(number)
                 
-    print('-'*50)
+    # print('-'*50)
+print(f'Total matches {match_counter}')
+print(f'Invalid ids {invalid_ids}')
+print(f'Sum of invalid ids {invalid_ids_sum}')
