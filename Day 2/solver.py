0d829144fc22e2bc9d28df48cb25e9dfaa9c4850
JuanDa <coziestwharf@gmail.com>
2025-12-03 20:00:54 -0500

    Test only for primes

--- Day 2/solver.py
+++ Day 2/solver.py
@@ -1,4 +1,5 @@
 import os
+from primePy import primes
 
 input = os.path.join(os.path.dirname(__file__), 'input.txt')
 with open(input) as f:
@@ -14,22 +15,33 @@ for id_range in list_ids[0]:
     start = int(id_range.split('-')[0])
     end = int(id_range.split('-')[1])
     id_range_list = range(start, end+1)
-    # print(f'Checking {id_range}')
+    print(f'Checking {id_range}')
     for number in id_range_list:
         number_str = str(number)
-        # print(f'Cheking {number}')
-        half_str = number_str[len(number_str)//2::]
-        if len(number_str) % 2 == 1:
-            pass
-        else:
-            pattern = number_str[:len(number_str)//2]
-            if half_str == pattern:
-                # print(f'Match found for {pattern} in {half_str}')
-                match_counter+=1
-                invalid_ids.append(number)
-                invalid_ids_sum+=int(number)
+        print(f'Cheking {number}')
+        if primes.check(len(number_str)):
+            pattern_str = number_str[0]
+            all_match = True
+            for digit in number_str[1:]:
+                if digit != pattern_str:
+                    all_match = False
+                    break
+            if all_match:
+                print("Match found")
+                match_counter += 1
+        # else:
+        #     pattern_str = number_str[0]
+        #     for digit in number_str[1:]:
+        #         print(f'Using pattern {pattern_str}')
+        #         if digit == pattern_str:
+        #             print(f'Match found')
+        #             match_counter+=1
+        #         else:
+        #             pattern_str+=digit
+            
                 
     # print('-'*50)
+
 print(f'Total matches {match_counter}')
 print(f'Invalid ids {invalid_ids}')
 print(f'Sum of invalid ids {invalid_ids_sum}')
