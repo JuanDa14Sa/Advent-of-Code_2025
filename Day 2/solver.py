75c363de9a4ba53ff1a772ee4865a39c8fb691d3
JuanDa <coziestwharf@gmail.com>
2025-12-04 18:16:01 -0500

    Day 2 part 2 completed

--- Day 2/solver.py
+++ Day 2/solver.py
@@ -1,5 +1,6 @@
 import os
-from primePy import primes
+import sympy as sp
+import tqdm
 
 input = os.path.join(os.path.dirname(__file__), 'input.txt')
 with open(input) as f:
@@ -10,16 +11,15 @@ for line in lines:
 
 match_counter = 0
 invalid_ids = []
-invalid_ids_sum = 0
-for id_range in list_ids[0]:
+for id_range in tqdm.tqdm(list_ids[0], desc="Processing ranges"):
     start = int(id_range.split('-')[0])
     end = int(id_range.split('-')[1])
     id_range_list = range(start, end+1)
-    print(f'Checking {id_range}')
+    # print(f'Checking {id_range}')
     for number in id_range_list:
         number_str = str(number)
-        print(f'Cheking {number}')
-        if primes.check(len(number_str)):
+        # print(f'Cheking {number}')
+        if sp.isprime(len(number_str)):
             pattern_str = number_str[0]
             all_match = True
             for digit in number_str[1:]:
@@ -27,21 +27,20 @@ for id_range in list_ids[0]:
                     all_match = False
                     break
             if all_match:
-                print("Match found")
+                # print("Match found")
+                invalid_ids.append(number)
                 match_counter += 1
-        # else:
-        #     pattern_str = number_str[0]
-        #     for digit in number_str[1:]:
-        #         print(f'Using pattern {pattern_str}')
-        #         if digit == pattern_str:
-        #             print(f'Match found')
-        #             match_counter+=1
-        #         else:
-        #             pattern_str+=digit
-            
-                
-    # print('-'*50)
+        else:
+            divisors = sp.proper_divisors(len(number_str))
+            for divisor in divisors:
+                pattern_str = number_str[:divisor]
+                times = len(number_str)//divisor
+                if pattern_str * times == number_str:
+                    # print('Match found')
+                    invalid_ids.append(number)
+                    match_counter+=1
+                    break
 
 print(f'Total matches {match_counter}')
 print(f'Invalid ids {invalid_ids}')
-print(f'Sum of invalid ids {invalid_ids_sum}')
+print(f'Sum of invalid ids {sum(invalid_ids)}')
