af7c0271b3901f49d02a8f350e313785b834cf1e
JuanDa <coziestwharf@gmail.com>
2025-12-06 18:16:38 -0500

    Day 3 completed

--- Day 3/solver.py
+++ Day 3/solver.py
@@ -9,11 +9,12 @@ with open(input) as f:
 max_list = []
 for bank in tqdm.tqdm(lines):
     max_string = ''
+    start = 0
     for k in range(12):
-        end = len(bank)-12-k
-        best_i = 0
+        end = len(bank)-(12-k)
+        best_i = start
         best_digit = bank[best_i]
-        for i in range(1, end+1):
+        for i in range(start, end+1):
             digit = bank[i]
             if digit > best_digit:
                 best_digit = digit
@@ -21,5 +22,6 @@ for bank in tqdm.tqdm(lines):
         max_string+=best_digit
         start = best_i+1
     print(max_string)
-    max_list.append(max_string)
-print(max_list)
+    # max_list.append(int(max_string))
+# print(max_list)
+print(sum(max_list))
